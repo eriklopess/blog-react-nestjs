c689196d6e2d00ee7962990561ad04968621add5
Erik Lopes <erik.lopesoliveira2019@gmail.com>
2022-09-27 15:55:58 -0300

    feat(Header): creating Header component with searchbar

---
+++ client/src/pages/Home/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './style.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+export default function index() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+  return (
+    <div className="header">
+      <div className="header--logo" />
+      <div className="header--search">
+        <form action="POST" className="header--search--form" onSubmit={handleSubmit}>
+          <label htmlFor="search" className="header--search--form--label">
+            <input type="text" placeholder="Search" autoComplete="off" name="search" />
+          </label>
+          <button type="submit" name="submit" className="header--search--form--submit">
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+        </form>
+      </div>
+      <div className="header--user" />
+    </div>
+  );
+}
