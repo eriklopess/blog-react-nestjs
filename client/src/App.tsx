288266d63b3b63b8e3902f6ccbf92aa4129153ad
Erik Lopes <erik.lopesoliveira2019@gmail.com>
2022-09-27 15:20:18 -0300

    inicializando projeto

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,10 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <div className="App" />
+  );
 }
 
-export default App
+export default App;
